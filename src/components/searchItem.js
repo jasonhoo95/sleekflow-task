9f03d9116299b748f5c189c2698922a4b1986e91
jasonhoo95 <hoochongyi95@gmail.com>
2022-10-04 22:18:15 +0800

    made some ui changes

--- src/components/searchItem.js
+++ src/components/searchItem.js
@@ -2,20 +2,24 @@ import { useEffect } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 export default function SearchItem({ data }) {
-	return data?.map((o, key) => {
-		return (
-			<div key={key} className="search-item">
-				<Link to={`/contact/${o.id}`}>
-					<div style={{ display: "flex" }}>
-						<h3>{key + 1}.</h3>
-						<img className="search-img" src={o.image} />
-						<div style={{ textAlign: "left", marginLeft: "30px" }}>
-							<h3>{o.name}</h3>
-							<div>{o.species}</div>
+	if (data?.length > 0) {
+		return data?.map((o, key) => {
+			return (
+				<div key={key} className="search-item">
+					<Link to={`/contact/${o.id}`}>
+						<div style={{ display: "flex" }}>
+							<h3>{key + 1}.</h3>
+							<img className="search-img" src={o.image} />
+							<div style={{ textAlign: "left", marginLeft: "30px" }}>
+								<h3>{o.name}</h3>
+								<div>{o.species}</div>
+							</div>
 						</div>
-					</div>
-				</Link>
-			</div>
-		);
-	});
+					</Link>
+				</div>
+			);
+		});
+	} else {
+		return <h3>NO RESULT!</h3>;
+	}
 }
